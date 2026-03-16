fd2a106b373e41d24703560cffb63aa967d01e2a
Reeqho <teeqho90@gmail.com>
2026-03-17 00:44:52 +0700

    woi ireng selesaiin front end nya

--- resources/views/fields/index.blade.php
+++ resources/views/fields/index.blade.php
@@ -1,4 +1,29 @@
 @extends('layouts.master')
 @section('content')
-    <h1>ini adalah halaman field</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Price per hour</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($fields as $field)
+    <tr>
+      <td>{{ $field->name }}</td>
+      <td>{{ $field->type }}</td>
+      <td>{{ $field->price_per_hour }}</td>
+      <td>{{ $field->description }}</td>
+    </tr> 
+    @endforeach --}}
+            <tr>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="nav-container">
     <a href="/" class="nav-logo">⚽ MiniSoccer</a>
     <ul class="nav-links">
-        <li><a href="/" class="active">Home</a></li>
-        <li><a href="/fields">Lapangan</a></li>
-        <li><a href="/bookings">Booking</a></li>
+        <li><a href="/" class="{{ request()->is('/') ? 'active' : '' }}">Home</a></li>
+        <li><a href="/fields" class="{{ request()->is('fields*') ? 'active' : '' }}">Lapangan</a></li>
+        <li><a href="/bookings" class="{{ request()->is('bookings*') ? 'active' : '' }}">Booking</a></li>
     </ul>
 </div>
